18d3af57ea8bc3954d89b4410c54c3e42a6f2ded
tanya0404 <149752404+tanya0404@users.noreply.github.com>
2025-03-06 11:47:27 +0530

    web dev

---
+++ JS/JAVASCRIPT/basics.js
@@ -0,0 +1,79 @@
+// console.log('hello js')
+// console.warn('warning')
+// console.error('error occor')
+// console.info('info')
+
+
+// alert('alert came')
+// confirm('are you sure')
+// var n=prompt('enter your age')
+// var a=Number(n)
+// console.log(a)
+
+                        //      == : compare only value
+                        //      === : compare value and datatype
+
+
+// var units= Number(prompt('enter units'))
+// if(units<50){
+//     console.log(units*5)
+// }
+// else if(units>50 && units<100){
+//     console.log(units*8)
+// }
+// else{
+//     console.log(units*10)
+// }
+
+
+// ternary operator      condition ? true : false
+
+// 12>10 ? console.log('true') : console.log('false')                
+
+                        /*       String Methods
+                                 .slice()
+                                 .split()
+                                 .replace()   and .replaceAll()
+                                 .include()
+                        */
+
+
+                                                // FUNCTIONS
+
+//                                            statement function
+
+// function add(){
+
+// }
+
+//                                           expression function
+
+// var add=function(a,b){
+
+// }
+
+//                                           anonymous function
+
+// function(){
+                                        
+// }
+
+
+
+
+
+
+
+
+// REMOVE DUPLICATE VALUES FROM ARRAY
+
+// var arr=[1,1,2,1,1,2,3,4,4,4,6,7,11,4,8];
+// var ans=[...new Set(arr)];
+// console.log(ans);
+
+
+// FIND SECOND LARGEST NUMBER IN ARRAY
+
+// var arr=[1,5,3,77,18,100,50,5,77];
+
+
